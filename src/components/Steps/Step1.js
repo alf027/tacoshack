37e1e84bd9134065e712e8295dfa4dc4ae5a3862
John Alfano <alf027@gmail.com>
2019-04-12 16:00:12 -0600

    configuring form will probably refactor

--- src/components/Steps/Step1.js
+++ src/components/Steps/Step1.js
@@ -1,7 +1,50 @@
 import React from 'react';
 
-const StepOne = () => {
-  return <div>this is step one</div>
+class StepOne extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  validateNext = () => {
+    this.props.next('test data')
+  }
+
+  validatePrev = () => {
+
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="ui form">
+          <div className="grouped fields">
+            <label for="tacoType">Select Taco Type</label>
+            <div className="field">
+              <div className="ui radio checkbox">
+                <input type="radio" name="tacoType"></input>
+                <label>Taco</label>
+              </div>
+            </div>
+            <div className="field">
+              <div className="ui radio checkbox">
+                <input type="radio" name="tacoType"></input>
+                <label>Burrito</label>
+              </div>
+            </div>
+            <div className="field">
+              <div className="ui radio checkbox">
+                <input type="radio" name="tacoType"></input>
+                <label>Bowl</label>
+              </div>
+            </div>
+          </div>
+        </div>
+        <button onClick={this.validateNext}>next</button>
+        <button onClick={this.validatePrev}>previous</button>
+      </div>
+
+    )
+  }
 }
 
 export default StepOne;
