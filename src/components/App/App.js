5ecec84843fe7f1fd961135c26086fc4081129ea
John Alfano <alf027@gmail.com>
2019-04-13 12:56:20 -0600

    more styling

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,8 +5,10 @@ import OrderWizard from '../OrderWizard/OrderWizard';
 class App extends Component {
   render() {
     return (
-      <div>
-        <div>Nicu's Taco Shack</div>
+      <div className="container">
+        <div className="text-center">
+          <div className="col col-xm-12">Nicu's Taco Shack</div>
+        </div>
         <OrderWizard />
       </div>
     )
