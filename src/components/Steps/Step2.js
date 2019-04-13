3bba4ef2007bbf089dbe0684f563bda2ddc5e42c
John Alfano <alf027@gmail.com>
2019-04-12 18:12:29 -0600

    adding second step

---
+++ src/components/Steps/Step2.js
@@ -0,0 +1,70 @@
+import React from 'react';
+
+class StepTwo extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      selectedMeat: { meatType: 'Chicken' },
+      meatTypesDef: [
+        { meatType: 'Chicken' },
+        { meatType: 'Beef' },
+        { meatType: 'No Meat' }
+      ]
+    }
+
+
+  }
+
+  validateNext = () => {
+    this.props.next(this.state.selectedMeat)
+  }
+
+  validatePrev = () => {
+    this.props.prev()
+  }
+
+  setMeatState = (event) => {
+    console.log(event.target.value)
+    const index = event.target.value
+    //utilizing the index as the value prop does not like objects
+    this.setState({ selectedTaco: this.state.meatTypesDef[index] }, () => {
+      console.log(this.state.selectedTaco);
+    })
+  }
+
+  buildRadioOptions = () => {
+    return this.state.meatTypesDef.map((meat, index) => {
+      return (
+        <div key={index} className="field">
+          <div className="ui radio checkbox" >
+            <input type="radio" name="tacoType" value={index} onClick={this.setMeatState}></input>
+            <label>{meat.meatType}</label>
+          </div>
+        </div>
+      )
+    });
+  }
+
+  render() {
+    if (this.props.currentStep !== 2) {
+      return null;
+    }
+    const meat = this.buildRadioOptions()
+
+    return (
+      <div>
+        <div className="ui form">
+          <div className="grouped fields">
+            <label htmlFor="tacoType">Select Meat Type</label>
+            {meat}
+          </div>
+        </div>
+        <button onClick={this.validateNext}>next</button>
+        <button onClick={this.validatePrev}>previous</button>
+      </div>
+
+    )
+  }
+}
+
+export default StepTwo
